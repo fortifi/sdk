1a34fec38af046e48089d9eb8801105fcb90548b
Lee Martin <lee@jdi.io>
2015-01-28 14:58:46 +0000

    Ref T387 domain model

--- src/Models/Domains/DomainModel.php
+++ src/Models/Domains/DomainModel.php
@@ -1,40 +1,150 @@
 <?php
 namespace Fortifi\Sdk\Models\Domains;
 
-use Fortifi\FortifiApi\Company\Responses;
+use Fortifi\FortifiApi\Domain\Endpoints\DomainEndpoint;
+use Fortifi\FortifiApi\Domain\Payloads\CreateDomainPayload;
+use Fortifi\FortifiApi\Domain\Payloads\DomainCompanyFidPayload;
+use Fortifi\FortifiApi\Domain\Payloads\PaginatedDomainsByCompanyFidPayload;
+use Fortifi\FortifiApi\Domain\Payloads\UpdateDomainPayload;
+use Fortifi\FortifiApi\Domain\Responses\DomainResponse;
+use Fortifi\FortifiApi\Domain\Responses\DomainsResponse;
+use Fortifi\FortifiApi\Foundation\Payloads\FidPayload;
+use Fortifi\FortifiApi\Foundation\Payloads\PaginatedDataNodePayload;
+use Fortifi\FortifiApi\Foundation\Responses\BoolResponse;
+use Fortifi\FortifiApi\Foundation\Responses\FidResponse;
 use Fortifi\Sdk\Models\Api\FortifiApiModel;
 
 class DomainModel extends FortifiApiModel
 {
-
-  public function create()
+  /**
+   * @param int    $limit
+   * @param int    $page
+   * @param string $sortField
+   * @param string $sortDirection
+   * @param int    $showDeleted
+   * @param string $filter
+   *
+   * @return DomainsResponse
+   */
+  public function all(
+    $limit = null, $page = null, $sortField = null, $sortDirection = null,
+    $showDeleted = null, $filter = null
+  )
   {
+    $payload                = new PaginatedDataNodePayload();
+    $payload->limit         = $limit;
+    $payload->page          = $page;
+    $payload->sortField     = $sortField;
+    $payload->sortDirection = $sortDirection;
+    $payload->showDeleted   = $showDeleted;
+    $payload->filter        = $filter;
+
+    $ep = DomainEndpoint::bound($this->getApi());
+    return $ep->all($payload)->get();
+  }
+
+  /**
+   * @param string $fid
+   *
+   * @return DomainResponse
+   */
+  public function retrieve($fid)
+  {
+    $payload      = new FidPayload();
+    $payload->fid = $fid;
+
+    $ep = DomainEndpoint::bound($this->getApi());
+    return $ep->retrieve($payload)->get();
+  }
+
+  /**
+   * @param string $domain
+   * @param string $companyFid
+   *
+   * @return FidResponse
+   */
+  public function create($domain, $companyFid)
+  {
+    $payload             = new CreateDomainPayload();
+    $payload->domain     = $domain;
+    $payload->companyFid = $companyFid;
+
+    $ep = DomainEndpoint::bound($this->getApi());
+    return $ep->create($payload)->get();
+  }
+
+  /**
+   * @param string $fid
+   * @param string $domain
+   *
+   * @return BoolResponse
+   */
+  public function update($fid, $domain)
+  {
+    $payload         = new UpdateDomainPayload();
+    $payload->fid    = $fid;
+    $payload->domain = $domain;
+
+    $ep = DomainEndpoint::bound($this->getApi());
+    return $ep->update($payload)->get();
+  }
+
+  /**
+   * @param string $fid
+   *
+   * @return BoolResponse
+   */
+  public function verify($fid)
+  {
+    $payload      = new FidPayload();
+    $payload->fid = $fid;
+
+    $ep = DomainEndpoint::bound($this->getApi());
+    return $ep->verify($payload)->get();
+  }
+
+  /**
+   * @param $fid
+   * @param $companyFid
+   *
+   * @return BoolResponse
+   */
+  public function setCompany($fid, $companyFid)
+  {
+    $payload             = new DomainCompanyFidPayload();
+    $payload->fid        = $fid;
+    $payload->companyFid = $companyFid;
+
+    $ep = DomainEndpoint::bound($this->getApi());
+    return $ep->setCompany($payload)->get();
+  }
+
+  /**
+   * @param $fid
+   *
+   * @return BoolResponse
+   */
+  public function unlinkCompany($fid)
+  {
+    $payload      = new FidPayload();
+    $payload->fid = $fid;
+
+    $ep = DomainEndpoint::bound($this->getApi());
+    return $ep->unlinkCompany($payload)->get();
 
   }
 
-  public function update()
+  /**
+   * @param $companyFid
+   *
+   * @return DomainsResponse
+   */
+  public function getByCompany($companyFid)
   {
+    $payload             = new PaginatedDomainsByCompanyFidPayload();
+    $payload->companyFid = $companyFid;
 
+    $ep = DomainEndpoint::bound($this->getApi());
+    return $ep->getByCompany($payload)->get();
   }
-
-  public function all()
-  {
-
-  }
-
-  public function current()
-  {
-
-  }
-
-  public function assign()
-  {
-
-  }
-
-  public function unassign()
-  {
-
-  }
-
 }
