d4195d1d3eb726c301bcb96659798256949c574a
Jleagle <jimeagle@gmail.com>
2015-02-03 11:00:14 +0000

    Affiliate stubs

---
+++ src/Models/Affiliate/AffiliateModel.php
@@ -0,0 +1,27 @@
+<?php
+namespace Fortifi\Sdk\Models\Affiliate;
+
+use Fortifi\FortifiApi\Affiliate\Responses\AffiliateResponse;
+use Fortifi\FortifiApi\Affiliate\Responses\AffiliatesResponse;
+use Fortifi\Sdk\Models\Api\FortifiApiModel;
+
+class AffiliateModel extends FortifiApiModel
+{
+
+  /**
+   * @return AffiliateResponse
+   */
+  public function retrieve()
+  {
+
+  }
+
+  /**
+   * @return AffiliatesResponse
+   */
+  public function all()
+  {
+
+  }
+
+}
