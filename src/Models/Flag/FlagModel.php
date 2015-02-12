77d8ac10ff618b6a5441070e0d1978c1bc3499fe
Tom Kay <oridan82@gmail.com>
2015-02-12 13:09:59 +0000

    add docblocks for FlagModel

--- src/Models/Flag/FlagModel.php
+++ src/Models/Flag/FlagModel.php
@@ -4,11 +4,18 @@ namespace Fortifi\Sdk\Models\Flag;
 use Fortifi\FortifiApi\Flag\Endpoints\FlagEndpoint;
 use Fortifi\FortifiApi\Flag\Payloads\CreateFlagPayload;
 use Fortifi\FortifiApi\Flag\Payloads\ListFlagsPayload;
+use Fortifi\FortifiApi\Flag\Responses\FlagsResponse;
 use Fortifi\FortifiApi\Foundation\Payloads\FidPayload;
+use Fortifi\FortifiApi\Foundation\Responses\BoolResponse;
 use Fortifi\Sdk\Models\Api\FortifiApiModel;
 
 class FlagModel extends FortifiApiModel
 {
+  /**
+   * @param $userFid
+   *
+   * @return FlagsResponse
+   */
   public function all($userFid)
   {
     $payload = new ListFlagsPayload();
@@ -18,6 +25,14 @@ class FlagModel extends FortifiApiModel
     return $ep->all($payload)->get();
   }
 
+  /**
+   * @param $userFid
+   * @param $objectFid
+   * @param $colour
+   * @param $notes
+   *
+   * @return BoolResponse
+   */
   public function create($userFid, $objectFid, $colour = null, $notes = null)
   {
     $payload = new CreateFlagPayload();
@@ -30,6 +45,11 @@ class FlagModel extends FortifiApiModel
     return $ep->create($payload)->get();
   }
 
+  /**
+   * @param $fid
+   *
+   * @return BoolResponse
+   */
   public function delete($fid)
   {
     $payload = new FidPayload();
