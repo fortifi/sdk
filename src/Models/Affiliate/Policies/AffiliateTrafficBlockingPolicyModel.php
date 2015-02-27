78243dd3e5d699853b5dd77aa5713c88645073df
Lee Martin <lee@jdi.io>
2015-02-27 17:10:24 +0000

    refactor AffiliateTrafficBlockingPolicyModel all method

--- src/Models/Affiliate/Policies/AffiliateTrafficBlockingPolicyModel.php
+++ src/Models/Affiliate/Policies/AffiliateTrafficBlockingPolicyModel.php
@@ -2,13 +2,13 @@
 namespace Fortifi\Sdk\Models\Affiliate\Policies;
 
 use Fortifi\FortifiApi\Affiliate\Endpoints\Policies\AffiliateTrafficBlockingPolicyEndpoint;
+use Fortifi\FortifiApi\Affiliate\Payloads\Policies\ListAffiliatePolicyPayload;
 use Fortifi\FortifiApi\Affiliate\Payloads\Policies\TrafficBlocking\CreateAffiliateTrafficBlockingPolicyPayload;
 use Fortifi\FortifiApi\Affiliate\Payloads\Policies\TrafficBlocking\UpdateAffiliateTrafficBlockingPolicyPayload;
 use Fortifi\FortifiApi\Affiliate\Responses\Policies\TrafficBlocking\AffiliateTrafficBlockingPoliciesResponse;
 use Fortifi\FortifiApi\Affiliate\Responses\Policies\TrafficBlocking\AffiliateTrafficBlockingPolicyResponse;
 use Fortifi\FortifiApi\Affiliate\Responses\Policies\TrafficBlocking\CreateAffiliateTrafficBlockingPolicyResponse;
 use Fortifi\FortifiApi\Foundation\Payloads\FidPayload;
-use Fortifi\FortifiApi\Foundation\Payloads\PaginatedDataNodePayload;
 use Fortifi\FortifiApi\Foundation\Requests\FortifiApiRequestInterface;
 use Fortifi\FortifiApi\Foundation\Responses\BoolResponse;
 use Fortifi\Sdk\Models\Api\FortifiApiModel;
@@ -16,27 +16,34 @@ use Fortifi\Sdk\Models\Api\FortifiApiModel;
 class AffiliateTrafficBlockingPolicyModel extends FortifiApiModel
 {
   /**
-   * @param int    $limit
-   * @param int    $page
-   * @param string $sortField
-   * @param string $sortDirection
-   * @param bool   $showDeleted
-   * @param string $filter
+   * @param string $companyFid;
+   * @param string $affiliateFid;
+   * @param string $foundationFid;
+   * @param string $sid1;
+   * @param string $sid2;
+   * @param string $sid3;
+   * @param string $action;
+   * @param string $country;
+   * @param string $platform;
    *
    * @return AffiliateTrafficBlockingPoliciesResponse|FortifiApiRequestInterface
    */
   public function all(
-    $limit = 10, $page = 1, $sortField = null,
-    $sortDirection = null, $showDeleted = false, $filter = null
+    $companyFid = null, $affiliateFid = null, $foundationFid = null,
+    $sid1 = null, $sid2 = null, $sid3 = null, $action = null,
+    $country = null, $platform = null
   )
   {
-    $payload = new PaginatedDataNodePayload();
-    $payload->limit = $limit;
-    $payload->page = $page;
-    $payload->sortField = $sortField;
-    $payload->sortDirection = $sortDirection;
-    $payload->showDeleted = $showDeleted;
-    $payload->filter = $filter;
+    $payload = new ListAffiliatePolicyPayload();
+    $payload->companyFid = $companyFid;
+    $payload->affiliateFid = $affiliateFid;
+    $payload->foundationFid = $foundationFid;
+    $payload->sid1 = $sid1;
+    $payload->sid2 = $sid2;
+    $payload->sid3 = $sid3;
+    $payload->action = $action;
+    $payload->country = $country;
+    $payload->platform = $platform;
 
     $ep = AffiliateTrafficBlockingPolicyEndpoint::bound($this->getApi());
     return $ep->all($payload)->get();
