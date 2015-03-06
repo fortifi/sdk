ceea6e1f9730a18ac343d2c76b40f0c0281a2310
Tom Kay <oridan82@gmail.com>
2015-03-06 09:06:56 +0000

    add affiliate pixel model

---
+++ src/Models/Affiliate/AffiliatePixelModel.php
@@ -0,0 +1,86 @@
+<?php
+namespace Fortifi\Sdk\Models\Affiliate;
+
+use Fortifi\FortifiApi\Affiliate\Endpoints\Policies\AffiliatePixelPolicyEndpoint;
+use Fortifi\FortifiApi\Affiliate\Payloads\Pixels\CreatePixelPolicyPayload;
+use Fortifi\FortifiApi\Affiliate\Payloads\Pixels\ListPixelPoliciesPayload;
+use Fortifi\FortifiApi\Affiliate\Payloads\Pixels\UpdatePixelPolicyPayload;
+use Fortifi\FortifiApi\Affiliate\Responses\Pixels\PixelPoliciesResponse;
+use Fortifi\FortifiApi\Foundation\Requests\FortifiApiRequestInterface;
+use Fortifi\FortifiApi\Foundation\Responses\BoolResponse;
+use Fortifi\Sdk\Models\Api\FortifiApiModel;
+
+class AffiliatePixelModel extends FortifiApiModel
+{
+  /**
+   * @param string $affiliateFid
+   *
+   * @return PixelPoliciesResponse|FortifiApiRequestInterface
+   */
+  public function all($affiliateFid)
+  {
+    $payload = new ListPixelPoliciesPayload();
+    $payload->affiliateFid = $affiliateFid;
+
+    $ep = AffiliatePixelPolicyEndpoint::bound($this->getApi());
+    return $ep->all($payload)->get();
+  }
+
+  /**
+   * @param string $affiliateFid
+   * @param string $action
+   * @param string $campaign
+   * @param string $sid1
+   * @param string $sid2
+   * @param string $sid3
+   * @param string $displayName
+   * @param string $pixelType
+   * @param string $url
+   * @param string $content
+   *
+   * @return BoolResponse|FortifiApiRequestInterface
+   */
+  public function create(
+    $affiliateFid, $action, $campaign, $sid1, $sid2, $sid3,
+    $displayName, $pixelType, $url, $content
+  )
+  {
+    $payload = new CreatePixelPolicyPayload();
+    $payload->resourceFid = $affiliateFid;
+    $payload->action = $action;
+    $payload->campaignHash = $campaign;
+    $payload->sid1 = $sid1;
+    $payload->sid2 = $sid2;
+    $payload->sid3 = $sid3;
+
+    $payload->displayName = $displayName;
+    $payload->pixelType = $pixelType;
+    $payload->url = $url;
+    $payload->content = $content;
+
+    $ep = AffiliatePixelPolicyEndpoint::bound($this->getApi());
+    return $ep->create($payload)->get();
+  }
+
+  /**
+   * @param string $pixelFid
+   * @param string $displayName
+   * @param string $pixelType
+   * @param string $url
+   * @param string $content
+   *
+   * @return BoolResponse|FortifiApiRequestInterface
+   */
+  public function update($pixelFid, $displayName, $pixelType, $url, $content)
+  {
+    $payload = new UpdatePixelPolicyPayload();
+    $payload->fid = $pixelFid;
+    $payload->displayName = $displayName;
+    $payload->pixelType = $pixelType;
+    $payload->url = $url;
+    $payload->content = $content;
+
+    $ep = AffiliatePixelPolicyEndpoint::bound($this->getApi());
+    return $ep->update($payload)->get();
+  }
+}
