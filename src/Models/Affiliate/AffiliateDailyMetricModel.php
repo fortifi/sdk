58e0a269ebc82fd1a6ade36adb3a50f2e45e978b
Lee Martin <lee@jdi.io>
2015-03-06 11:05:08 +0000

    update daily metrics model

--- src/Models/Affiliate/AffiliateDailyMetricModel.php
+++ src/Models/Affiliate/AffiliateDailyMetricModel.php
@@ -3,62 +3,18 @@ namespace Fortifi\Sdk\Models\Affiliate;
 
 use Fortifi\FortifiApi\Affiliate\Endpoints\AffiliateDailyMetricEndpoint;
 use Fortifi\FortifiApi\Affiliate\Payloads\DailyMetrics\ListAffiliateDailySummaryPayload;
+use Fortifi\FortifiApi\Affiliate\Payloads\DailyMetrics\ListAffiliateMetricsDurationPayload;
 use Fortifi\FortifiApi\Affiliate\Payloads\DailyMetrics\ListAffiliateStatsDurationPayload;
-use Fortifi\FortifiApi\Affiliate\Payloads\DailyMetrics\RetrieveAffiliateDailyMetricPayload;
-use Fortifi\FortifiApi\Affiliate\Responses\DailyMetrics\AffiliateDailyMetricResponse;
-use Fortifi\FortifiApi\Affiliate\Responses\DailyMetrics\AffiliateDailyMetricsResponse;
+use Fortifi\FortifiApi\Affiliate\Responses\DailyMetrics\AffiliateConversionMetricsResponse;
 use Fortifi\FortifiApi\Affiliate\Responses\DailyMetrics\AffiliateDailySummaryResponse;
+use Fortifi\FortifiApi\Affiliate\Responses\DailyMetrics\AffiliateFinanceMetricsResponse;
 use Fortifi\FortifiApi\Affiliate\Responses\DailyMetrics\AffiliateSalesOverviewResponse;
 use Fortifi\FortifiApi\Affiliate\Responses\DailyMetrics\AffiliateTopAffiliatesResponse;
-use Fortifi\FortifiApi\Foundation\Payloads\PaginatedDataNodePayload;
 use Fortifi\FortifiApi\Foundation\Requests\FortifiApiRequestInterface;
 use Fortifi\Sdk\Models\Api\FortifiApiModel;
 
 class AffiliateDailyMetricModel extends FortifiApiModel
 {
-  /**
-   * @param int    $limit
-   * @param int    $page
-   * @param string $sortField
-   * @param string $sortDirection
-   * @param bool   $showDeleted
-   * @param string $filter
-   *
-   * @return AffiliateDailyMetricsResponse|FortifiApiRequestInterface
-   */
-  public function all(
-    $limit = 10, $page = 1, $sortField = null,
-    $sortDirection = null, $showDeleted = false, $filter = null
-  )
-  {
-    $payload = new PaginatedDataNodePayload();
-    $payload->limit = $limit;
-    $payload->page = $page;
-    $payload->sortField = $sortField;
-    $payload->sortDirection = $sortDirection;
-    $payload->showDeleted = $showDeleted;
-    $payload->filter = $filter;
-
-    $ep = AffiliateDailyMetricEndpoint::bound($this->getApi());
-    return $ep->all($payload)->get();
-  }
-
-  /**
-   * @param string $affiliateFid
-   * @param string $date
-   *
-   * @return AffiliateDailyMetricResponse|FortifiApiRequestInterface
-   */
-  public function retrieve($affiliateFid, $date)
-  {
-    $payload = new RetrieveAffiliateDailyMetricPayload();
-    $payload->affiliateFid = $affiliateFid;
-    $payload->date = $date;
-
-    $ep = AffiliateDailyMetricEndpoint::bound($this->getApi());
-    return $ep->retrieve($payload)->get();
-  }
-
   /**
    * @param string $affiliateFid
    * @param int    $summariseDays
@@ -127,5 +83,33 @@ class AffiliateDailyMetricModel extends FortifiApiModel
 
     $ep = AffiliateDailyMetricEndpoint::bound($this->getApi());
     return $ep->salesOverview($payload)->get();
+  }
+
+  /**
+   * @param int $days
+   *
+   * @return AffiliateConversionMetricsResponse|FortifiApiRequestInterface
+   */
+  public function conversionMetrics($days = 30)
+  {
+    $payload = new ListAffiliateMetricsDurationPayload();
+    $payload->days = $days;
+
+    $ep = AffiliateDailyMetricEndpoint::bound($this->getApi());
+    return $ep->conversionMetrics($payload)->get();
+  }
+
+  /**
+   * @param int $days
+   *
+   * @return AffiliateFinanceMetricsResponse|FortifiApiRequestInterface
+   */
+  public function financeMetrics($days = 30)
+  {
+    $payload = new ListAffiliateMetricsDurationPayload();
+    $payload->days = $days;
+
+    $ep = AffiliateDailyMetricEndpoint::bound($this->getApi());
+    return $ep->financeMetrics($payload)->get();
   }
 }
