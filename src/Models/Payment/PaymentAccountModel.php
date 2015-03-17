823b531b7ddbd566aabc714777e357756d420a67
Brooke Bryan <brooke@bajb.net>
2015-03-17 10:35:11 +0000

    Payment Service and Payment Account Models

--- src/Models/Payment/PaymentAccountModel.php
+++ src/Models/Payment/PaymentAccountModel.php
@@ -1,21 +1,121 @@
 <?php
 namespace Fortifi\Sdk\Models\Payment;
 
+use Fortifi\FortifiApi\Foundation\Payloads\FidPayload;
+use Fortifi\FortifiApi\Foundation\Responses\BoolResponse;
+use Fortifi\FortifiApi\Payment\Endpoints\PaymentAccountEndpoint;
+use Fortifi\FortifiApi\Payment\Payloads\Account\CreatePaymentAccountPayload;
+use Fortifi\FortifiApi\Payment\Payloads\Account\GetPaymentAccountPayload;
+use Fortifi\FortifiApi\Payment\Payloads\SetConfigurationPayload;
+use Fortifi\FortifiApi\Payment\Responses\Account\PaymentAccountResponse;
+use Fortifi\FortifiApi\Payment\Responses\Account\PaymentAccountsResponse;
 use Fortifi\Sdk\Models\Api\FortifiApiModel;
 
 class PaymentAccountModel extends FortifiApiModel
 {
-  public function create($ownerFid, $serviceFid, $configuration) { }
+  /**
+   * @param $ownerFid
+   * @param $serviceFid
+   * @param $configuration
+   *
+   * @return PaymentAccountResponse
+   */
+  public function create($ownerFid, $serviceFid, $configuration)
+  {
+    $payload = new CreatePaymentAccountPayload();
+    $payload->ownerFid = $ownerFid;
+    $payload->serviceFid = $serviceFid;
+    $payload->configuration = $configuration;
 
-  public function all($ownerFid) { }
+    $ep = PaymentAccountEndpoint::bound($this->getApi());
+    return $ep->create($payload)->get();
+  }
 
-  public function getByProcessor($processor) { }
+  /**
+   * @param $ownerFid
+   *
+   * @return PaymentAccountsResponse
+   */
+  public function all($ownerFid)
+  {
+    $payload = new FidPayload();
+    $payload->fid = $ownerFid;
 
-  public function retrieve($accountFid) { }
+    $ep = PaymentAccountEndpoint::bound($this->getApi());
+    return $ep->all($payload)->get();
+  }
 
-  public function archive($accountFid) { }
+  /**
+   * @param $ownerFid
+   * @param $processor
+   *
+   * @return PaymentAccountsResponse
+   */
+  public function getByProcessor($ownerFid, $processor)
+  {
+    $payload = new GetPaymentAccountPayload();
+    $payload->fid = $ownerFid;
+    $payload->processor = $processor;
 
-  public function restore($accountFid) { }
+    $ep = PaymentAccountEndpoint::bound($this->getApi());
+    return $ep->getByProcessor($payload)->get();
+  }
 
-  public function setConfiguration($accountFid, $configuration) { }
+  /**
+   * @param $accountFid
+   *
+   * @return PaymentAccountResponse
+   */
+  public function retrieve($accountFid)
+  {
+    $payload = new FidPayload();
+    $payload->fid = $accountFid;
+
+    $ep = PaymentAccountEndpoint::bound($this->getApi());
+    return $ep->retrieve($payload)->get();
+  }
+
+  /**
+   * @param $accountFid
+   *
+   * @return BoolResponse
+   */
+  public function archive($accountFid)
+  {
+    $payload = new FidPayload();
+    $payload->fid = $accountFid;
+
+    $ep = PaymentAccountEndpoint::bound($this->getApi());
+    return $ep->archive($payload)->get();
+  }
+
+  /**
+   * @param $accountFid
+   *
+   * @return BoolResponse
+   */
+  public function restore($accountFid)
+  {
+    $payload = new FidPayload();
+    $payload->fid = $accountFid;
+
+    $ep = PaymentAccountEndpoint::bound($this->getApi());
+    return $ep->restore($payload)->get();
+  }
+
+  /**
+   * @param $accountFid
+   * @param $configuration
+   *
+   * @return BoolResponse
+   */
+  public function setConfiguration($accountFid, $configuration)
+  {
+    $payload = new SetConfigurationPayload();
+    $payload->fid = $accountFid;
+    $payload->configuration = $configuration;
+
+    $ep = PaymentAccountEndpoint::bound($this->getApi());
+    return $ep->setConfiguration($payload)->get();
+  }
 }
--- src/Models/Payment/PaymentServiceModel.php
+++ src/Models/Payment/PaymentServiceModel.php
@@ -1,44 +1,226 @@
 <?php
 namespace Fortifi\Sdk\Models\Payment;
 
+use Fortifi\FortifiApi\Foundation\Payloads\FidPayload;
+use Fortifi\FortifiApi\Foundation\Payloads\GetByEnumPayload;
+use Fortifi\FortifiApi\Foundation\Payloads\PaginatedDataNodePayload;
+use Fortifi\FortifiApi\Foundation\Payloads\ToggleFidPayload;
+use Fortifi\FortifiApi\Foundation\Responses\BoolResponse;
+use Fortifi\FortifiApi\Payment\Endpoints\PaymentServiceEndpoint;
 use Fortifi\FortifiApi\Payment\Enums\PaymentServiceDirection;
+use Fortifi\FortifiApi\Payment\Enums\PaymentServiceProcessor;
+use Fortifi\FortifiApi\Payment\Payloads\Services\CreatePaymentServicePayload;
+use Fortifi\FortifiApi\Payment\Payloads\Services\SetPaymentServiceRequiredFormsPayload;
+use Fortifi\FortifiApi\Payment\Payloads\SetConfigurationPayload;
+use Fortifi\FortifiApi\Payment\Responses\Services\PaymentServiceResponse;
+use Fortifi\FortifiApi\Payment\Responses\Services\PaymentServicesResponse;
 use Fortifi\Sdk\Models\Api\FortifiApiModel;
 
 class PaymentServiceModel extends FortifiApiModel
 {
+  /**
+   * @param       $name
+   * @param       $direction
+   * @param       $processor
+   * @param       $configuration
+   * @param bool  $selectable
+   * @param array $requiredForms
+   * @param bool  $enabled
+   *
+   * @return PaymentServiceResponse
+   */
   public function create(
     $name, $direction, $processor, $configuration, $selectable = false,
-    $enabled = true, array $requiredForms = []
+    array $requiredForms = [], $enabled = true
   )
   {
+    $payload = new CreatePaymentServicePayload();
+    $payload->name = $name;
+    $payload->direction = $direction;
+    $payload->processor = $processor;
+    $payload->configuration = $configuration;
+    $payload->selectable = $selectable;
+    $payload->requiredForms = $requiredForms;
+    $payload->enabled = $enabled;
+
+    $ep = PaymentServiceEndpoint::bound($this->getApi());
+    return $ep->create($payload)->get();
   }
 
+  /**
+   * @param null $limit
+   * @param null $page
+   * @param null $sortField
+   * @param null $sortDirection
+   * @param null $showDeleted
+   * @param null $filter
+   *
+   * @return PaymentServicesResponse
+   */
   public function all(
     $limit = null, $page = null, $sortField = null, $sortDirection = null,
     $showDeleted = null, $filter = null
   )
   {
+    $payload = new PaginatedDataNodePayload();
+    $payload->limit = $limit;
+    $payload->page = $page;
+    $payload->sortField = $sortField;
+    $payload->sortDirection = $sortDirection;
+    $payload->showDeleted = $showDeleted;
+    $payload->filter = $filter;
+
+    $ep = PaymentServiceEndpoint::bound($this->getApi());
+    return $ep->all($payload)->get();
   }
 
-  public function getByProcessor($processor) { }
+  /**
+   * @param string $processor
+   *
+   * @return PaymentServicesResponse
+   */
+  public function getByProcessor(
+    $processor = PaymentServiceProcessor::BLACKHOLE
+  )
+  {
+    $payload = new GetByEnumPayload();
+    $payload->value = $processor;
 
+    $ep = PaymentServiceEndpoint::bound($this->getApi());
+    return $ep->getByProcessor($payload)->get();
+  }
+
+  /**
+   * @param string $direction
+   *
+   * @return PaymentServicesResponse
+   */
   public function getByDirection(
     $direction = PaymentServiceDirection::INBOUND
   )
   {
+    $payload = new GetByEnumPayload();
+    $payload->value = $direction;
+
+    $ep = PaymentServiceEndpoint::bound($this->getApi());
+    return $ep->getByDirection($payload)->get();
   }
 
-  public function retrieve($serviceFid) { }
+  /**
+   * @param $serviceFid
+   *
+   * @return PaymentServiceResponse
+   */
+  public function retrieve($serviceFid)
+  {
+    $payload = new FidPayload();
+    $payload->fid = $serviceFid;
 
-  public function archive($serviceFid) { }
+    $ep = PaymentServiceEndpoint::bound($this->getApi());
+    return $ep->retrieve($payload)->get();
+  }
 
-  public function restore($serviceFid) { }
+  /**
+   * @param $serviceFid
+   *
+   * @return BoolResponse
+   */
+  public function archive($serviceFid)
+  {
+    $payload = new FidPayload();
+    $payload->fid = $serviceFid;
 
-  public function enable($serviceFid) { }
+    $ep = PaymentServiceEndpoint::bound($this->getApi());
+    return $ep->archive($payload)->get();
+  }
 
-  public function disable($serviceFid) { }
+  /**
+   * @param $serviceFid
+   *
+   * @return BoolResponse
+   */
+  public function restore($serviceFid)
+  {
+    $payload = new FidPayload();
+    $payload->fid = $serviceFid;
 
-  public function setSelectable($serviceFid, $selectable = true) { }
+    $ep = PaymentServiceEndpoint::bound($this->getApi());
+    return $ep->restore($payload)->get();
+  }
 
-  public function setConfiguration($serviceFid, $configuration) { }
+  /**
+   * @param $serviceFid
+   *
+   * @return BoolResponse
+   */
+  public function enable($serviceFid)
+  {
+    $payload = new FidPayload();
+    $payload->fid = $serviceFid;
+
+    $ep = PaymentServiceEndpoint::bound($this->getApi());
+    return $ep->enable($payload)->get();
+  }
+
+  /**
+   * @param $serviceFid
+   *
+   * @return BoolResponse
+   */
+  public function disable($serviceFid)
+  {
+    $payload = new FidPayload();
+    $payload->fid = $serviceFid;
+
+    $ep = PaymentServiceEndpoint::bound($this->getApi());
+    return $ep->disable($payload)->get();
+  }
+
+  /**
+   * @param      $serviceFid
+   * @param bool $selectable
+   *
+   * @return BoolResponse
+   */
+  public function setSelectable($serviceFid, $selectable = true)
+  {
+    $payload = new ToggleFidPayload();
+    $payload->fid = $serviceFid;
+    $payload->state = $selectable;
+
+    $ep = PaymentServiceEndpoint::bound($this->getApi());
+    return $ep->setSelectable($payload)->get();
+  }
+
+  /**
+   * @param $serviceFid
+   * @param $configuration
+   *
+   * @return BoolResponse
+   */
+  public function setConfiguration($serviceFid, $configuration)
+  {
+    $payload = new SetConfigurationPayload();
+    $payload->fid = $serviceFid;
+    $payload->configuration = $configuration;
+
+    $ep = PaymentServiceEndpoint::bound($this->getApi());
+    return $ep->setConfiguration($payload)->get();
+  }
+
+  /**
+   * @param       $serviceFid
+   * @param array $forms
+   *
+   * @return BoolResponse
+   */
+  public function setRequiredForms($serviceFid, array $forms = [])
+  {
+    $payload = new SetPaymentServiceRequiredFormsPayload();
+    $payload->fid = $serviceFid;
+    $payload->requiredForms = $forms;
+
+    $ep = PaymentServiceEndpoint::bound($this->getApi());
+    return $ep->setRequiredForms($payload)->get();
+  }
 }
