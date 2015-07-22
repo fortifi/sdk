9b7c937ddc1f63627d18f583fd56bc98e66b50fd
Brooke Bryan <brooke@bajb.net>
2015-07-22 10:50:47 +0100

    Support setting of customer properties

--- src/Models/AbstractCustomer.php
+++ src/Models/AbstractCustomer.php
@@ -4,6 +4,9 @@ namespace Fortifi\Sdk\Models;
 use Fortifi\FortifiApi\Customer\Endpoints\CustomerEndpoint;
 use Fortifi\FortifiApi\Customer\Payloads\CustomerEmailPayload;
 use Fortifi\FortifiApi\Customer\Payloads\CustomerPhonePayload;
+use Fortifi\FortifiApi\Property\Endpoints\PropertyEndpoint;
+use Fortifi\FortifiApi\Property\Payloads\SetPropertyValuePayload;
+use Packaged\Helpers\ValueAs;
 
 abstract class AbstractCustomer extends FortifiModel
 {
@@ -31,7 +34,7 @@ abstract class AbstractCustomer extends FortifiModel
     $payload->setAsDefault = $asDefault;
     $ep = $this->_getEndpoint();
     $this->_processRequest($ep->addPhone($payload));
-    return;
+    return $this;
   }
 
   public function addEmailAddress($emailAddress, $asDefault = false)
@@ -41,7 +44,8 @@ abstract class AbstractCustomer extends FortifiModel
     $payload->email = $emailAddress;
     $payload->setAsDefault = $asDefault;
     $ep = $this->_getEndpoint();
-    return $this->_processRequest($ep->addEmail($payload));
+    $this->_processRequest($ep->addEmail($payload));
+    return $this;
   }
 
   /**
@@ -50,5 +54,89 @@ abstract class AbstractCustomer extends FortifiModel
   protected function _getEndpoint()
   {
     return CustomerEndpoint::bound($this->_getApi());
+  }
+
+  /**
+   * @return PropertyEndpoint
+   */
+  protected function _getPropertyEndpoint()
+  {
+    return PropertyEndpoint::bound($this->_getApi());
+  }
+
+  public function setFlag($name, $value = true)
+  {
+    if(empty($this->_customerFid))
+    {
+      throw new \RuntimeException(
+        "You cannot set a flag before setting a customer fid"
+      );
+    }
+
+    $payload = new SetPropertyValuePayload();
+    $payload->fid = $this->_customerFid;
+    $payload->property = $name;
+    $payload->value = ValueAs::bool($value);
+    $this->_processRequest($this->_getPropertyEndpoint()->setFlag($payload));
+
+    return $this;
+  }
+
+  public function setValue($name, $value)
+  {
+    if(empty($this->_customerFid))
+    {
+      throw new \RuntimeException(
+        "You cannot set a value before setting a customer fid"
+      );
+    }
+
+    $payload = new SetPropertyValuePayload();
+    $payload->fid = $this->_customerFid;
+    $payload->property = $name;
+    $payload->value = ValueAs::string($value);
+    $this->_processRequest($this->_getPropertyEndpoint()->setValue($payload));
+
+    return $this;
+  }
+
+  public function incrementCounter($name, $value = 1)
+  {
+    if(empty($this->_customerFid))
+    {
+      throw new \RuntimeException(
+        "You cannot increment a counter  before setting a customer fid"
+      );
+    }
+
+    $payload = new SetPropertyValuePayload();
+    $payload->fid = $this->_customerFid;
+    $payload->property = $name;
+    $payload->value = ValueAs::int($value, 1);
+    $this->_processRequest(
+      $this->_getPropertyEndpoint()->incrementCounter($payload)
+    );
+
+    return $this;
+  }
+
+  public function decrementCounter($name, $value = 1)
+  {
+    if(empty($this->_customerFid))
+    {
+      throw new \RuntimeException(
+        "You cannot decrement a counter before setting a customer fid"
+      );
+    }
+
+    $payload = new SetPropertyValuePayload();
+    $payload->fid = $this->_customerFid;
+    $payload->property = $name;
+    $payload->value = ValueAs::int($value, 1);
+    $this->_processRequest(
+      $this->_getPropertyEndpoint()->decrementCounter($payload)
+    );
+
+    return $this;
   }
 }
