2708014c133fac64d00ae713b8b15489a74ed868
Brooke Bryan <brooke@bajb.net>
2015-08-14 11:33:40 +0100

    Email address unsubscribe

---
+++ src/Models/EmailAddress.php
@@ -0,0 +1,41 @@
+<?php
+namespace Fortifi\Sdk\Models;
+
+use Fortifi\FortifiApi\Contact\Endpoints\EmailEndpoint;
+use Fortifi\FortifiApi\Contact\Enums\UnsubscribeType;
+use Fortifi\FortifiApi\Contact\Payloads\Status\UnsubscribeEmailPayload;
+
+class EmailAddress extends FortifiModel
+{
+  protected $_emailAddress;
+
+  /**
+   * @return mixed
+   */
+  public function getEmailAddress()
+  {
+    return $this->_emailAddress;
+  }
+
+  /**
+   * @param $emailAddress
+   *
+   * @return $this
+   */
+  public function setEmailAddress($emailAddress)
+  {
+    $this->_emailAddress = $emailAddress;
+    return $this;
+  }
+
+  public function unsubscribe($groupFid = null)
+  {
+    $payload = new UnsubscribeEmailPayload();
+    $payload->emailAddress = $this->_emailAddress;
+    $payload->groupFid = $groupFid;
+    $payload->type = UnsubscribeType::UNSUBSCRIBE_HARD;
+
+    $endpoint = EmailEndpoint::bound($this->_getApi());
+    return $this->_processRequest($endpoint->unsubscribe($payload));
+  }
+}
